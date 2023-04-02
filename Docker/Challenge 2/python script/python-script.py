0c320410a90ef20c6df2f29a1ea8e79e7582ec1c
Marc <mherranz98@gmail.com>
2023-04-02 18:57:34 +0200

    Postgres part addition to Challenge 2

--- Docker/Challenge 2/python script/python-script.py
+++ Docker/Challenge 2/python script/python-script.py
@@ -1,7 +1,8 @@
 import logging
 from kafka import KafkaConsumer
 from pymongo import MongoClient
-import psycopg2
+from string import Template
+from psycopg2 import connect as Psycopg2Client
 import json
 
 # Set logging parameters for future monitoring, debugging or error-handling
@@ -39,8 +40,8 @@ def connectMongoDB(host, port, username, password):
 def connectPostgreSQL(host, port, database, username, password):
     """Function to connect to PostgreSQL"""
     try:
-        connection = psycopg2.connect(host=host, port=port, database=database,
-                                      username=username, password=password)
+        connection = Psycopg2Client(host=host, port=port, database=database,
+                                    user=username, password=password)
         cursor = connection.cursor()
         logging.info("Connected to PostgreSQL")
         return {"connection": connection, "cursor": cursor}
@@ -49,7 +50,7 @@ def connectPostgreSQL(host, port, database, username, password):
 
 
 def createMongoCollection(client, db_name, collection_name):
-    """Function to create database and collection. These will not be created until first 
+    """Function to create database and collection. These will not be created until first
     message is inserted"""
     try:
         db = client[db_name]
@@ -60,18 +61,16 @@ def createMongoCollection(client, db_name, collection_name):
         logging.error("Unable to create Mongo database and collection")
 
 
-def createTablePostgreSQL(PostgresClient, database_name, table_name, table_schema):
+def createTablePostgreSQL(PostgresClient, table_name, table_schema):
     """Function to create a table in PostgreSQL if not exists"""
-    query1 = "CREATE DATABASE IF NOT EXISTS {0}".format(database_name)
-    query2 = "CREATE TABLE IF NOT EXISTS {0} ({1})".format(
+    query = "CREATE TABLE IF NOT EXISTS public.{0} ({1})".format(
         table_name, table_schema)
     try:
-        PostgresClient.cursor.execute(query1)
-        PostgresClient.cursor.execute(query2)
-        PostgresClient.connection.commit()
-        logging.info("Database and table created")
+        PostgresClient["cursor"].execute(query)
+        PostgresClient["connection"].commit()
+        logging.info("Table created")
     except:
-        logging.error("Unable to create database or table")
+        logging.error("Unable to create table")
 
 
 def writeMessageToMongo(message, collection):
@@ -79,20 +78,22 @@ def writeMessageToMongo(message, collection):
     try:
         decoded_msg = json.loads(message.value.decode("utf-8"))
         collection.insert_one(decoded_msg)
-        logging.info("Quote inserted")
+        logging.info("Quote successfully inserted in MongoDB")
     except:
         logging.error("Unable to insert the message to Mongo database")
 
 
-def writeMessageToPostgreSQL(message, PostgresClient, database_name, table_name):
+def writeMessageToPostgreSQL(message, PostgresClient):
     """Function to insert a single message into PostreSQL defined in previous function"""
     decoded_msg = json.loads(message.value.decode("utf-8"))
-    query = "INSERT INTO public.quotes(quotes, charact, imag) VALUES ('{0}','{1}','{2}')".format(
-        decoded_msg['quote'], decoded_msg['character'], decoded_msg['image'])
+    template = Template(
+        """INSERT INTO public.quotes(quotes, charact, imag) VALUES ('$quote' , '$charact' , '$imag')""")
+    query = template.substitute(
+        quote=decoded_msg['quote'].replace("'", "`"), charact=decoded_msg['character'], imag=decoded_msg['image'])
     try:
-        PostgresClient.cursor.execute(query)
-        PostgresClient.connection.commit()
-        logging.info("Quote inserted")
+        PostgresClient["cursor"].execute(query)
+        PostgresClient["connection"].commit()
+        logging.info("Quote successfully inserted in Postgres DB")
     except:
         logging.error("Unable to insert the message to PostgreSQL database")
 
@@ -112,16 +113,19 @@ def main():
     host = "localhost"
     port = 27017  # Listener internal/external to Docker (9092/29092)
     username = "mchi"
-    password = 'mchi1234'
-    database_name = "simpson_db"
+    password = "mchi1234"
+    database_name = "simpsons"
+
+    # Connect to Postgres with same authentication of Mongo and create table (schema on-write)
+    port_postgres = "5432"
     table_name = "quotes"
-    table_schema = """ quote_id SERIAL, 
-        quotes VARCHAR(500),
-        character VARCHAR(500),
-        image VARCHAR(500)
+    table_schema = """ quote_id SERIAL PRIMARY KEY,
+	quotes VARCHAR(500),
+	charact VARCHAR(500),
+	imag VARCHAR(500)
         """
 
-    """
+    # Connect to MongoDB
     mongoClient = connectMongoDB(
         host=host, port=port, username=username, password=password)
 
@@ -129,17 +133,22 @@ def main():
     db_name = "simpson_db"
     collection_name = "quotes"
     collection = createMongoCollection(mongoClient, db_name, collection_name)
-    """
 
-    postgresClient = connectPostgreSQL(
-        host=host, port=port, database=database_name, username=username, password=password)
-
-    createTablePostgreSQL(postgresClient, database_name=database_name,
-                          table_name=table_name, table_schema=table_schema)
+    # Create Postgres table in public part
+    connection = connectPostgreSQL(host=host, port=port_postgres, database=database_name,
+                                   username=username, password=password)
+    createTablePostgreSQL(connection, table_name=table_name,
+                          table_schema=table_schema)
 
     # Insert messages in stream while connected to kafka bootstrap server (kafka_consumer)
-    # for msg in kafka_consumer:
-    #writeMessageToMongo(msg, collection)
+    for msg in kafka_consumer:
+        if msg.offset % 2 != 0:  # for every message we get two entries: content + metadata
+            writeMessageToMongo(msg, collection)
+            writeMessageToPostgreSQL(msg, connection)
+        else:
+            pass
+
+    connection["cursor"].close()
 
 
 if __name__ == "__main__":
